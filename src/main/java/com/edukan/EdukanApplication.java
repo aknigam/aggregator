6af9e1fc678b1e301af8411f8061557bc201524b
Anand Nigam <anand.k.nigam@gmail.com>
2016-10-02 12:34:46 +0530

    initial project setup

---
+++ src/main/java/com/edukan/EdukanApplication.java
@@ -0,0 +1,12 @@
+package com.edukan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdukanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdukanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/edukan/EdukanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edukan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EdukanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
